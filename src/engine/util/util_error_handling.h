88b9863f49a2ba583af3fe7f85d575c4aeae9510
Aaron Gonzalez <31080326+arxeous@users.noreply.github.com>
2022-09-02 15:22:16 -0700

    Error handling function rename, and appropriate macro definitions for target OS

--- src/engine/util/util_error_handling.h
+++ src/engine/util/util_error_handling.h
@@ -1,14 +1,25 @@
 #ifndef BIRDMAZEGAME_UTIL_ERROR_HANDLING_H
 #define BIRDMAZEGAME_UTIL_ERROR_HANDLING_H
 
+#ifndef NDEBUG
+    #ifndef MSVC
+        #include <signal.h>
+        #define DEBUG_BREAK raise(SIGTRAP)
+    #else
+        #define DEBUG_BREAK __debugbreak()
+    #endif                                      // MSVC
+#else
+    #define DEBUG_BREAK 0
+#endif                                          // DEBUG
+
 #include <assert.h>
 #include <SDL.h>
 
 // breaks into debug mode on false value. 
-#define ASSERT(x) if (!(x)) __debugbreak();
+#define ASSERT(x) if (!(x)) DEBUG_BREAK;
 
 // executes function saves its success flags and checks it against our assert
-#define SDLCall(x)                                          \
+#define SDLErrorHandle(x)                                          \
     {                                                       \
         SDL_ClearError();                                   \
         int flag = x;                                       \
@@ -16,7 +27,7 @@
     }
 
 // for SDL function that return null as opposed to integer flags 
-#define SDLCallNull(x)                                      \
+#define SDLErrorHandleNull(x)                                      \
 {                                                           \
    int flag = -1 ? x == NULL : 0;                           \
    ASSERT(SDLLogCall(#x, __FILE__, __LINE__, flag))         \
